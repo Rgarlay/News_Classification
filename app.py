c4855d6bcc6cb86cbd2eb2f6eb5771743db7c5b5
Rgarlay <ravigarlay@gmail.com>
2025-07-15 18:50:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,22 +7,25 @@ application = Flask(__name__)
 
 app = application
 
-@app.route('/', methods = ['GET','POST'])
+@app.route('/', methods=['GET', 'POST'])
 def predict_news():
-    if request.method=='GET':
+    if request.method == 'GET':
         return render_template('index.html')
     else:
-        data = CustomData(
-            text = str(request.form.get('news_text'))
-        )
+        try:
+            data = CustomData(
+                text=str(request.form.get('news_text'))
+            )
+            pred = data.get_as_dataframe()
+            logging.info(f'The dataframe is given as {pred}')
 
-        pred = data.get_as_dataframe()
-        print(pred)
-        logging.info(f'The dataframe is given as {pred}')
+            pred_pipeline = PredictPipeline()
+            results = pred_pipeline.predict(pred)
 
-        pred_pipeline = PredictPipeline()
-        results = pred_pipeline.predict(pred)
-        return render_template('index.html',results=results)
+            return render_template('index.html', results=results)
+        except Exception as e:
+            logging.error(f"Prediction error: {e}")
+            return render_template('index.html', results="Something went wrong.")
     
 if __name__=="__main__":
     app.run(host="0.0.0.0",debug=True)
